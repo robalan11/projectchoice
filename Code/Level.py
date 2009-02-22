04957f507f5109d4fe24c15a7bb544586775a4bb
kalvorod <kalvorod@a40a4324-f2ef-11dd-9fdb-3debd5008f28>
2009-02-22 21:58:46 +0000

    Fixed level reader to read in basic level structure.
    Does not yet draw anything.

--- Code/Level.py
+++ Code/Level.py
@@ -1,10 +1,9 @@
-
 class Level(object):
-    def __init__(levelfile):
-        self.loadLevelfile(levelfile)
-        self.run()
+    def __init__(self,levelfile):
+        self.level=[]
+        self.loadLevelfile(levelfile)	
 
-    def loadLevelfile(levelfile):
+    def loadLevelfile(self,levelfile):
         grid = open(levelfile, 'r').readlines()
         for row in grid:
             rooms = row.split(',')
@@ -12,7 +11,10 @@ class Level(object):
                 self.level.append(Room(room))
 
 class Room(object):
-    def __init__(room):
+    def __init__(self,room):
         self.WestWall = room[0]
         self.NorthWall = room[1]
         self.Floor = room[2]
+        print self.WestWall, self.NorthWall, self.Floor
+        
+level = Level("1.txt")
