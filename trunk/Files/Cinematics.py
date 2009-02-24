5bdeff28077b15616f0a285501b04a3fe762d073
robalan <robalan@a40a4324-f2ef-11dd-9fdb-3debd5008f28>
2009-02-24 20:35:30 +0000

    move, turn, and speak events made.

--- trunk/Files/Cinematics.py
+++ trunk/Files/Cinematics.py
@@ -5,26 +5,89 @@ import sys
 #import Panda libraries
 import direct.directbase.DirectStart
 from direct.task import Task
+from direct.gui.OnscreenText import OnscreenText
+from pandac.PandaModules import TextNode
 
 class Event(object):
-    def __init__(self, time, type, remainder):
+    def __init__(self, time, type, duration, remainder):
         self.time = float(time)
         self.type = type
-        self.duration = float(remainder[0])
+        self.duration = float(duration)
+        
         if self.type == 'm':
-            self.function = self.waiter
+            self.function = self.move
+            self.target = int(remainder[0])
+            self.points = []
+            for point in remainder[1:]:
+                nums = point.split(',')
+                self.points.append((float(nums[0]), float(nums[1]), float(nums[2])))
+        
+        elif self.type == 't':
+            self.function = self.turn
+            self.target = int(remainder[0])
+            self.rotation = float(remainder[1])
+            #self.startrot = self.target.getH()
+            self.startrot = 0
+        
+        elif self.type == 's':
+            self.function = self.speak
+            self.first = True
+            self.target = int(remainder[0])
+            self.file = remainder[1]
+        
         elif self.type == 'q':
             self.function = self.quit
     
-    def waiter(self, title):
+    def move(self, title):
         elapsed = time.clock()-self.time
         if elapsed < self.duration:
-            print title, elapsed
+            point = self.bezier(self.points[:], elapsed/self.duration)
+            #target.setPos(point)
+            print point
             return Task.cont
+        return Task.cont
+    
+    def turn(self, title):
+        elapsed = time.clock()-self.time
+        if elapsed < self.duration:
+            rot = self.startrot + self.rotation * (elapsed/self.duration)
+            #self.target.setH(rot)
+            print rot
+            return Task.cont
+        return Task.done
+    
+    def speak(self, title):
+        elapsed = time.clock()-self.time
+        if self.first:
+            text = open(self.file, 'r').readlines()
+            #put text on the screen
+            pos = 1
+            self.textlines = []
+            for line in text:
+                pos -= 0.06
+                self.textlines.append(OnscreenText(text=line, style=1, fg=(1,1,1,1), pos=(0, pos), align=TextNode.ACenter, scale = .06))
+            self.first = False
+        if elapsed < self.duration:
+            return Task.cont
+        for line in self.textlines:
+            pass
+            #line.clearText()
         return Task.done
     
     def quit(self, title):
         sys.exit(12345)
+    
+    def bezier(self, points, t):
+        if len(points) == 1:
+            return points[0]
+        next_itr = []
+        for i in xrange(len(points) - 1):
+            pt_a = [points[i][0],   points[i][1],   points[i][2]]
+            pt_b = [points[i+1][0], points[i+1][1], points[i+1][2]]
+            next_itr.append((pt_a[0] + float(pt_b[0] - pt_a[0]) * t,
+                             pt_a[1] + float(pt_b[1] - pt_a[1]) * t,
+                             pt_a[2] + float(pt_b[2] - pt_a[2]) * t))
+        return self.bezier(next_itr, t)
 
 class Cinematic(object):
     def __init__(self, cinefile):
@@ -38,7 +101,7 @@ class Cinematic(object):
         events = open(cinefile, 'r').readlines()
         for event in events:
             parts = event.split()
-            self.events.append(Event(parts[0], parts[1], parts[2:]))
+            self.events.append(Event(parts[0], parts[1], parts[2], parts[3:]))
     
     def runCin(self, title):
         elapsed = time.clock()-self.start
