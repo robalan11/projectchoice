49c369d0dff835e7bc631e162a8012eeb8054438
kalvorod <kalvorod@a40a4324-f2ef-11dd-9fdb-3debd5008f28>
2009-04-13 02:23:03 +0000

    Editor has a massive update!

--- trunk/Files/leveledit/menus.py
+++ trunk/Files/leveledit/menus.py
@@ -28,51 +28,78 @@ class GUI:
         self.text=pygame.font.SysFont('Arial', 18, False, False)
         self.bigtext=pygame.font.SysFont('Arial Black', 30, False, False)
         #Create the towerbuttons
+        
         towerbutton((self.x+1*32, 1*32), [0,'F'])
         towerbutton((self.x+2*32, 1*32), [0,'u'])
         towerbutton((self.x+3*32, 1*32), [0,'d'])
         towerbutton((self.x+4*32, 1*32), [0,'l'])
         towerbutton((self.x+5*32, 1*32), [0,'r'])
-        towerbutton((self.x+6*32, 1*32), [0,'2'])
-        towerbutton((self.x+1*32, 2*32), [0,'1'])
+        towerbutton((self.x+6*32, 1*32), [0,'g'])
+        
+        towerbutton((self.x+1*32, 2*32), [0,'h'])
         towerbutton((self.x+2*32, 2*32), [0,'a'])
         towerbutton((self.x+3*32, 2*32), [0,'b'])
         towerbutton((self.x+4*32, 2*32), [0,'c'])
         towerbutton((self.x+5*32, 2*32), [0,'e'])
         
         towerbutton((self.x+1*32, 3*32), [1,'W'])
-        towerbutton((self.x+2*32, 3*32), [1,'D'])
-        towerbutton((self.x+3*32, 3*32), [1,'S'])
-        towerbutton((self.x+4*32, 3*32), [1,'x'])
-        towerbutton((self.x+5*32, 3*32), [1,'e'])
-        towerbutton((self.x+6*32, 3*32), [1,'t'])
+        towerbutton((self.x+2*32, 3*32), [1,'A'])
+        towerbutton((self.x+3*32, 3*32), [1,'B'])
+        towerbutton((self.x+4*32, 3*32), [1,'C'])
         
-        towerbutton((self.x+1*32, 5*32), [2,'W'])
-        towerbutton((self.x+2*32, 5*32), [2,'D'])
-        towerbutton((self.x+3*32, 5*32), [2,'S'])
-        towerbutton((self.x+4*32, 5*32), [2,'x'])
-        towerbutton((self.x+5*32, 5*32), [2,'e'])
-        towerbutton((self.x+6*32, 5*32), [2,'t'])
+        towerbutton((self.x+1*32, 4*32), [2,'D'])
+        towerbutton((self.x+2*32, 4*32), [2,'S'])
         
-        towerbutton((self.x+1*32, 9*32), [4,'A'])
-        towerbutton((self.x+2*32, 9*32), [4,'B'])
-        towerbutton((self.x+3*32, 9*32), [4,'C'])
-        towerbutton((self.x+4*32, 9*32), [4,'D'])
-        towerbutton((self.x+5*32, 9*32), [4,'E'])
-        towerbutton((self.x+6*32, 9*32), [4,'F'])
-        towerbutton((self.x+7*32, 9*32), [4,'G'])
-        towerbutton((self.x+8*32, 9*32), [4,'H'])
+        towerbutton((self.x+1*32, 5*32), [3,'W'])
+        towerbutton((self.x+2*32, 5*32), [3,'A'])
+        towerbutton((self.x+3*32, 5*32), [3,'B'])
+        towerbutton((self.x+4*32, 5*32), [3,'C'])
         
-        towerbutton((self.x+1*32, 11*32), [5,'A'])
-        towerbutton((self.x+2*32, 11*32), [5,'B'])
-        towerbutton((self.x+3*32, 11*32), [5,'C'])
-        towerbutton((self.x+4*32, 11*32), [5,'D'])
-        towerbutton((self.x+5*32, 11*32), [5,'E'])
+        towerbutton((self.x+1*32, 6*32), [4,'D'])
+        towerbutton((self.x+2*32, 6*32), [4,'S'])
         
-        towerbutton((self.x+1*32, 13*32), [6,'1'])
-        towerbutton((self.x+2*32, 13*32), [6,'2'])
-        towerbutton((self.x+3*32, 13*32), [6,'3'])
-        towerbutton((self.x+4*32, 13*32), [6,'4'])
+        towerbutton((self.x+1*32, 8*32), [5,'0'])
+        towerbutton((self.x+2*32, 8*32), [5,'1'])
+        towerbutton((self.x+3*32, 8*32), [5,'2'])
+        towerbutton((self.x+4*32, 8*32), [5,'3'])
+        
+        towerbutton((self.x+1*32, 9*32), [8,'A'])
+        towerbutton((self.x+2*32, 9*32), [8,'B'])
+        towerbutton((self.x+3*32, 9*32), [8,'C'])
+        towerbutton((self.x+4*32, 9*32), [8,'D'])
+        towerbutton((self.x+5*32, 9*32), [8,'E'])
+        towerbutton((self.x+6*32, 9*32), [8,'F'])
+        towerbutton((self.x+7*32, 9*32), [8,'G'])
+        towerbutton((self.x+8*32, 9*32), [8,'H'])
+        
+        towerbutton((self.x+1*32, 10*32), [7,'0'])
+        towerbutton((self.x+2*32, 10*32), [7,'1'])
+        towerbutton((self.x+3*32, 10*32), [7,'2'])
+        towerbutton((self.x+4*32, 10*32), [7,'3'])
+        
+        towerbutton((self.x+1*32, 11*32), [10,'A'])
+        towerbutton((self.x+2*32, 11*32), [10,'B'])
+        towerbutton((self.x+3*32, 11*32), [10,'C'])
+        towerbutton((self.x+4*32, 11*32), [10,'D'])
+        towerbutton((self.x+5*32, 11*32), [10,'E'])
+        towerbutton((self.x+6*32, 11*32), [10,'F'])
+        towerbutton((self.x+7*32, 11*32), [10,'G'])
+        towerbutton((self.x+8*32, 11*32), [10,'H'])
+        
+        towerbutton((self.x+1*32, 13*32), [12,'1'])
+        towerbutton((self.x+2*32, 13*32), [12,'2'])
+        towerbutton((self.x+3*32, 13*32), [12,'3'])
+        towerbutton((self.x+4*32, 13*32), [12,'4'])
+        
+        towerbutton((self.x+1*32, 15*32), [14,'P'])
+        towerbutton((self.x+2*32, 15*32), [14,'G'])
+        towerbutton((self.x+3*32, 15*32), [14,'Q'])
+        towerbutton((self.x+4*32, 15*32), [14,'H'])
+        
+        towerbutton((self.x+1*32, 16*32), [13,'0'])
+        towerbutton((self.x+2*32, 16*32), [13,'1'])
+        towerbutton((self.x+3*32, 16*32), [13,'2'])
+        towerbutton((self.x+4*32, 16*32), [13,'3'])
         
     def draw(self, screen):
         for button in towerbutton.towerbuttons:
@@ -110,22 +137,8 @@ class towerbutton(pygame.sprite.Sprite):
         self.pos=pos
         pygame.sprite.Sprite.__init__(self)
         #pygame.sprite.Sprite.__init__()
-        if type==[2,'W']:
-            self.image=pygame.image.load('data/images/BuildTop.png')
-        elif type==[2,'D']:
-            self.image=pygame.image.load('data/images/BuildDoorTop.png')
-        elif type==[2,'S']:
-            self.image=pygame.image.load('data/images/BuildSlideTop.png')           
-        elif type==[1,'D']:
-            self.image=pygame.image.load('data/images/BuildDoorLeft.png')
-        elif type==[1,'S']:
-            self.image=pygame.image.load('data/images/BuildSlideLeft.png')
-        elif type==[1,'W']:
-            self.image=pygame.image.load('data/images/BuildLeft.png')
-        elif type==[0,'F']:
-            self.image=pygame.image.load('data/images/Floor.png')
-        else:
-            self.image=pygame.image.load( "./data/images/%d%s%s" %( type[0] , type[1] , ".png") )
+        self.image=pygame.image.load( "./data/images/%d%s%s" %( type[0] , type[1] , ".png") )
+        
         towerbutton.towerbuttons.add(self)
         self.rect=pygame.rect.Rect(pos, (32,32))
     def mouse_on(self):
