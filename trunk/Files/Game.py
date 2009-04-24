0e75856bf6a3c3ffbf0920d9f4fa03f0f2579bb1
kalvorod <kalvorod@a40a4324-f2ef-11dd-9fdb-3debd5008f28>
2009-04-24 11:54:35 +0000

    Made Barricades unpassable.

--- trunk/Files/Game.py
+++ trunk/Files/Game.py
@@ -24,7 +24,7 @@ class World(DirectObject):
         
         #Execute level construction code
         
-        self.level=Level("leveledit/Level3A.txt",self.player,"P") 
+        self.level=Level("leveledit/Level2.txt",self.player,"P") 
         self.player.setLevel(self.level)
         
         #Create dummy targets with a collision hull attached to them. No collision handler.
